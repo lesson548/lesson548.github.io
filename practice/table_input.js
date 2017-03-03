8d544b96f3387e8b8db88047355a31701237815d
lesson548 <465639748@qq.com>
2017-03-03 16:05:44 +0800

    table input remove

--- practice/table_input.js
+++ practice/table_input.js
@@ -11,8 +11,19 @@ $(function () {
         var abc = $("#add").val();
         var abc2 = $("#add2").val();
         $("table").append("<tr><td>" + abc + "</td><td>"+abc2+"</td><td><div class='del'>删除</div></td></tr>");
-    })
-    $(document).on("click",".del",function(e){
+    });
+    $(document).on("click",".del",function(){
         $(this).parent("td").parent("tr").remove();
-    })
-})
+    });
+
+    $(document).ready(function(){
+        $('.player > tbody tr:even').css('background','#cecd8f');
+        $('.player > tbody tr:odd').css('background','#97a5e9');
+    });
+
+    $(document).on("click","#insert",function(){
+        $('.player > tbody tr:even').css('background','#cecd8f');
+        $('.player > tbody tr:odd').css('background','#97a5e9');
+    });
+
+});
