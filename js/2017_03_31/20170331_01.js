cdffb98cf53f1d3db3bf9d068bd87a1a15f3a2e6
lesson548 <465639748@qq.com>
2017-07-31 16:20:18 +0800

    all

--- js/2017_03_31/20170331_01.js
+++ js/2017_03_31/20170331_01.js
@@ -13,24 +13,33 @@ function createRandom(num ,from ,to )
 
 
     $(function () {                        //图片切换
-//            $(".bg1").length;
+        // $('.bg2').show();
+        //
+        // $('.bg').click(function () {
+        //     $('.bg').hide();
+        //     $('.bg').show();
+        //     $(this).toggle(1000);
+        // });
+
         $('.bg').addClass('bg1');
         $('.bg').click(function () {
             $(this).toggleClass('bg2');
+            this.value=createRandom(1 ,1 ,9 );
+            $(this).html("<div class='math'>"+this.value+"</div>");
         });
+        //
+        // // change(1,1,9);
+        // function change(n,min,max) {                  //给每一张牌赋值随机数
+        //     this.value=createRandom(n ,min ,max );
+        //     $(this).html("<div class='math'>"+this.value+"</div>");
+        // }
+        //
+        //
+        // $('#chain').click(function () {         //刷新按钮
+        //     change(1,1,9);
+        // });
 
-        change(1,1,9);
 
-        $('#chain').click(function () {         //刷新按钮
-            change(1,1,9);
-        });
-
-        function change(n,min,max) {                  //给每一张牌赋值随机数
-            $(".bg").each(function(a){
-                this.value=createRandom(n ,min ,max );
-                $(this).html("<div class='math'>"+this.value+"</div>");
-            });
-        }
 
     });
 
